584624da6881b42b5d6886a74dac5646b751eef1
Gleb Ilinykh <gilinykh@gramant.ru>
2018-12-05 17:59:21 +0300

    Cleanup unused imports

--- src/main/java/com/gramant/starterdemo/StarterDemoConfiguration.java
+++ src/main/java/com/gramant/starterdemo/StarterDemoConfiguration.java
@@ -1,31 +1,17 @@
 package com.gramant.starterdemo;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.MapperFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.gramant.auth.adapters.rest.request.UserRegistrationRequest;
-import com.gramant.auth.app.ManageUser;
 import com.gramant.auth.app.RoleProvider;
 import com.gramant.auth.domain.PrivilegeId;
 import com.gramant.auth.domain.PrivilegedRole;
 import com.gramant.auth.domain.RoleId;
-import com.gramant.auth.domain.User;
 import com.gramant.auth.domain.event.UserCreatedEvent;
-import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
-import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.transaction.event.TransactionPhase;
-import org.springframework.transaction.event.TransactionalEventListener;
 
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Map;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
